1d4a28f998225dd2f0da8c1e41bb84d0a86a4080
NeunZehn <ookuro19@github.com>
2017-11-27 19:39:39 +0800

    Find the key to call click event
    InputManager.Instance.PushFallbackInputHandler(gameObject);

--- Assets/HoloToolkit-Examples/GazeRuler/Scripts/MeasureManager.cs
+++ Assets/HoloToolkit-Examples/GazeRuler/Scripts/MeasureManager.cs
@@ -13,8 +13,9 @@ namespace HoloToolkit.Examples.GazeRuler
     /// <summary>
     /// manager all measure tools here
     /// </summary>
-    public class MeasureManager : Singleton<MeasureManager>, IHoldHandler, IInputClickHandler
+    public class MeasureManager : Singleton<MeasureManager>, IInputClickHandler  , IHoldHandler
     {
+     
         private IGeometry manager;
         public GeometryMode Mode;
 
@@ -104,11 +105,21 @@ namespace HoloToolkit.Examples.GazeRuler
             // Nothing to do
         }
 
-    public void OnInputClicked(InputClickedEventData eventData)
-    {
-        OnSelect();
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            if (eventData.selectedObject)
+            {
+                Debug.Log(eventData.selectedObject.name);
+
+            }
+            else
+            {
+                Debug.Log("selectedObject is null");
+            }
+           // OnSelect();
+        }
     }
-}
+    
 
     public class Point
     {
---
+++ Assets/HoloToolkit-Examples/GazeRuler/Scripts/MeasureManagerTST.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+using System;
+using HoloToolkit.Unity.InputModule;
+
+namespace HoloToolkit.Examples.GazeRuler
+{
+    /// <summary>
+    /// manager all measure tools here
+    /// </summary>
+    public class MeasureManagerTST : MonoBehaviour, IInputClickHandler
+    {
+        private void Start()
+        {
+            InputManager.Instance.PushFallbackInputHandler(gameObject);
+        }
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            if (eventData.selectedObject)
+            {
+                Debug.Log(eventData.selectedObject.name);
+
+            }
+            else
+            {
+                Debug.Log("selectedObject is null");
+            }
+        }
+    }
+}
