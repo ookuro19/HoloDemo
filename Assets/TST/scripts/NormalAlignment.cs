bf90a09546b1a3439e10f6fe77afe10d28dffe15
NeunZehn <ookuro19@github.com>
2017-11-29 15:10:49 +0800

    Maybe the last time to commit
    I also need to identify the necessary scripts for build a holo-session.

--- Assets/TST/scripts/NormalAlignment.cs
+++ Assets/TST/scripts/NormalAlignment.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalAlignment : MonoBehaviour {
+public class NormalAlignment : MonoBehaviour
+{
 
     public GameObject alignedCube;
     private Camera mainCamera;
     private Ray ray;
     private RaycastHit hitInfo;
     private Vector3 alignVector;
-    
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         mainCamera = Camera.main;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButton(0))
         {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hitInfo))
             {
-                //使
+                //使薄片的Z轴与射线检测的法向量平行
                 alignedCube.transform.rotation = Quaternion.FromToRotation(Vector3.forward, hitInfo.normal);
                 Debug.DrawRay(hitInfo.point, hitInfo.normal, Color.red);
                 Debug.Log(string.Format("{0},{1},{2}", hitInfo.normal.x, hitInfo.normal.y, hitInfo.normal.z));
