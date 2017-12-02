b0cba08c4ddb69e5feeb3603ada9fd29974174e0
NeunZehn <ookuro19@github.com>
2017-12-02 11:14:43 +0800

    Repair the scene ClickTST

---
+++ Assets/TST/scripts/MeasureManagerTST.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using HoloToolkit.Unity.InputModule;
+
+public class MeasureManagerTST : MonoBehaviour, IInputClickHandler
+{
+	public GameObject tstCube;
+
+	private void Start()
+	{
+		InputManager.Instance.PushFallbackInputHandler(gameObject);
+	}
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+		if (eventData.selectedObject)
+		{
+			Debug.Log(eventData.selectedObject.name);
+
+		}
+		else
+		{
+			Debug.Log("selectedObject is null");
+			tstCube.SetActive(!tstCube.activeSelf);
+		}
+	}
+}
